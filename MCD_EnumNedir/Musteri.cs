b6b8e30e822b85d9d24b0216ae7024e4a93cc9b0
jackup <yk.58@hotmail.com>
2021-12-07 16:20:29 +0300

    code changed

---
+++ MCD_EnumNedir/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EnumNedir
+{
+    public class Musteri
+    {
+        public int id { get; set; }
+        public string musteriNumara { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string emailAdres { get; set; }
+    }
+}
